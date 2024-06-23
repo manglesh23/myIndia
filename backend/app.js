44f18088254aa322ad49b1d270274d6bd60e219d
manglesh23 <mangleshyadav2@gmail.com>
2024-06-23 20:48:21 +0530

    middleware added

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,12 @@ const exp = require("constants");
 const express = require("express");
 const { connectDatabase } = require("./databaseConnection/databaseConnect");
 const { router } = require("./authrouter/authrouter");
+
 const app = express();
 app.use(express.json());
+
 app.use("/",router)
+
 connectDatabase().then(() => {
   app.listen(8000, () => {
     console.log("listingin at 6000");
