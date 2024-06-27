6549780e92502f8acf74e1762975b7b5300e0455
manglesh23 <mangleshyadav2@gmail.com>
2024-06-27 15:27:10 +0530

    router changes

--- backend/authrouter/authrouter.js
+++ backend/authrouter/authrouter.js
@@ -1,14 +1,26 @@
 const express=require('express');
 const router=express.Router();
-const {createUser, userSignIn}=require('../controllers/users');
-const { createproduct, getProducts } = require('../controllers/product');
+
+const userController=require('../controllers/users');
+
+
+const productController=require('../controllers/product');
+
 const authMiddleware = require('../middleware/middleware');
-const { getOrders, createOrder } = require('../controllers/order');
-router.route('/createuser').post(createUser);
-router.route('/signin').get(userSignIn);
-router.route('/createproduct').post(createproduct);
-router.route('/getproducts').get(authMiddleware,getProducts);
-router.route('/getorder').get(getOrders);
-router.route('/createorder').get(authMiddleware,createOrder);
+
+
+const orderController=require('../controllers/order');
+
+router.route('/createuser').post(userController.createUser);
+
+router.route('/signin').get(userController.userSignIn);
+
+router.route('/createproduct').post(productController.createproduct);
+
+router.route('/getproducts').get(authMiddleware,productController.getProducts);
+
+router.route('/getorder').get(orderController.getOrders);
+
+router.route('/createorder').get(authMiddleware,orderController.createOrder);
 
 module.exports={router}
